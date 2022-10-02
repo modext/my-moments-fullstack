e9576511fe55d90d2639b2e9c40dcb3465b91154
Your Name <33578391+modext@users.noreply.github.com>
2022-10-02 22:28:16 +0100

    added cors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+async function registerUser(e) {
+  e.preventDefault()
+
+  const response = await fetch('http://localhost:3004/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      email, 
+      password,
+    })
+  })
+
+  const data = await response.json();
+  console.log(data)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Register</h1>
+     <form action="submit" onSubmit={registerUser}>
+        <label className="label">Name</label>
+        <input 
+          onChange={(e =>{setName(e.target.value)})} 
+          className="input"
+          value={name} 
+          type="text"
+          placeholder='Name' 
+        />
+ 
+        <label className="label">Email</label>
+        <input 
+          onChange={(e =>{setEmail(e.target.value)})} 
+          className="input"
+          value={email} 
+          type="email"
+          placeholder='Email' 
+        />
+ 
+        <label className="label">Password</label>
+        <input 
+          onChange={(e =>{setPassword(e.target.value)})}
+          className="input"
+          value={password} 
+          type="password"
+          placeholder='Password' 
+        />
+ 
+        <button onClick='' className="btn" type="submit">
+          Submit
+        </button>
+     </form>
     </div>
   );
 }
