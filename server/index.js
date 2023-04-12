229e5180b9ae4b249af716c79e4bb5d379bd29ee
modext <chigomorji@gmail.com>
2023-04-12 23:56:21 +0100

    dep

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
+
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,10 +17,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://modest:modest@cluster0.fhrjhju.mongodb.net/?retryWrites=true&w=majority"; 
+// const CONNECTION_URL = "mongodb+srv://modest:modest@cluster0.fhrjhju.mongodb.net/?retryWrites=true&w=majority"; 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
